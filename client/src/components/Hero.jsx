6c80bf4c25058149ab52e6e7e8311ac932510e9b
Aaron Harris <aaronsharris@gmail.com>
2022-01-18 11:39:55 -0600

    added useEffect for slider, added timeout function, and conditionals for images

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import { Button } from "../styles/Button.styles";
 import {
   HeroSection,
@@ -18,36 +18,63 @@ const Hero = ({ slides }) => {
   const length = slides.length;
   const timeout = useRef(null);
 
+  useEffect(() => {
+    const nextSlide = () => {
+      setCurrent((current) => (current === length - 1 ? 0 : current + 1));
+    };
+
+    timeout.current = setTimeout(nextSlide, 3000);
+
+    return function () {
+      if (timeout.current) {
+        clearTimeout(timeout.current);
+      }
+    };
+  }, [current, length]);
+
   const nextSlide = () => {
+    if (timeout.current) {
+      clearTimeout(timeout.current);
+    }
     setCurrent(current === length - 1 ? 0 : current + 1);
   };
 
   const prevSlide = () => {
+    if (timeout.current) {
+      clearTimeout(timeout.current);
+    }
     setCurrent(current === 0 ? length - 1 : current - 1);
   };
+
+  if (!Array.isArray(slides) || slides.length <= 0) {
+    return null;
+  }
   return (
     <HeroSection>
       <HeroWrapper>
         {slides.map((slide, index) => {
           return (
-            <HeroSlide key={index}> {index===current && (<HeroSlider>
-                <HeroImage src={slide.image} alt={slide.alt} />
-                <HeroContent>
-                  <h1>{slide.title}</h1>
-                  <p>{slide.price}</p>
-                  <Button
-                    to={slide.path}
-                    primary="true"
-                    css={`
-                      max-width: 160px;
-                    `}
-                  >
-                    {slide.label}
-                    <Arrow />
-                  </Button>
-                </HeroContent>
-              </HeroSlider>)}
-
+            <HeroSlide key={index}>
+              {" "}
+              {index === current && (
+                <HeroSlider>
+                  <HeroImage src={slide.image} alt={slide.alt} />
+                  <HeroContent>
+                    <h1>{slide.title}</h1>
+                    <p>{slide.price}</p>
+                    <Button
+                      to={slide.path}
+                      primary="true"
+                      css={`
+                        max-width: 160px;
+                      `}
+                    >
+                      {slide.label}
+                      <Arrow />
+                    </Button>
+                  </HeroContent>
+                </HeroSlider>
+              )}
             </HeroSlide>
           );
         })}
